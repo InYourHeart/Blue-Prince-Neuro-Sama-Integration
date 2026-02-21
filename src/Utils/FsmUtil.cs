913f1905c45853df27afbb5d453ad8039a97ac1f
InYourHeart <kinkfest111@gmail.com>
2026-02-21 12:15:26 +0000

    New FsmUtil features

--- src/Utils/FsmUtil.cs
+++ src/Utils/FsmUtil.cs
@@ -1,4 +1,6 @@
-﻿using Il2Cpp;
+﻿using Blue_Prince_Neuro_Sama_Integration_Mod.src.Managers;
+using Il2Cpp;
+using Il2CppHutongGames.PlayMaker;
 using Il2CppTMPro;
 using MelonLoader;
 using UnityEngine;
@@ -75,5 +77,118 @@ namespace Blue_Prince_Neuro_Sama_Integration_Mod.src.Utils
                 return null;
             }
         }
+
+		public static void ListFSMVariables(string gameObjectName)
+		{
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) {
+				Melon<Core>.Logger.Msg($"Could not list FSMVariables for {gameObjectName} because no PlayMakerFSM was found");
+				return;
+			}
+			
+			Melon<Core>.Logger.Msg($"Listing all FSMVariables for {gameObjectName}");
+			
+			ListFSMInts(gameObjectName);
+			ListFSMBools(gameObjectName);
+			ListFSMStrings(gameObjectName);
+			ListFSMGameObjects(gameObjectName);
+		}
+
+		public static void ListFSMInts(string gameObjectName)
+		{
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return;
+
+			for (int i = 0; i < fsm.FsmVariables.intVariables.Count; i++)
+			{
+				FsmInt fsmInt = fsm.FsmVariables.intVariables[i];
+
+				if (fsmInt != null) Melon<Core>.Logger.Msg($"{gameObjectName} - FsmInt - {i} - {fsmInt.name} - {fsmInt.value}");
+			}
+		}
+
+		public static void ListFSMBools(string gameObjectName)
+		{
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return;
+
+			for (int i = 0; i < fsm.FsmVariables.boolVariables.Count; i++)
+			{
+				FsmBool fsmBool = fsm.FsmVariables.boolVariables[i];
+
+				if (fsmBool != null) Melon<Core>.Logger.Msg($"{gameObjectName} - FsmBool - {i} - {fsmBool.name} - {fsmBool.value}");
+			}
+		}
+
+		public static void ListFSMStrings(string gameObjectName)
+		{
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return;
+
+			for (int i = 0; i < fsm.FsmVariables.stringVariables.Count; i++)
+			{
+				FsmString fsmString = fsm.FsmVariables.stringVariables[i];
+
+				if (fsmString != null) Melon<Core>.Logger.Msg($"{gameObjectName} - FsmString - {i} - {fsmString.name} - {fsmString.value}");
+			}
+		}
+
+		public static void ListFSMGameObjects(string gameObjectName)
+		{
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return;
+
+			for (int i = 0; i < fsm.FsmVariables.stringVariables.Count; i++)
+			{
+				FsmGameObject fsmGameObject = fsm.FsmVariables.gameObjectVariables[i];
+
+				if (fsmGameObject != null) Melon<Core>.Logger.Msg($"{gameObjectName} - FsmGameObject - {i} - {fsmGameObject.name}");
+			}
+		}
+
+		public static void ListFSMStates(string gameObjectName) {
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return;
+
+			for (int i = 0; i < fsm.FsmStates.Count; i++)
+			{
+				FsmState state = fsm.FsmStates[i];
+
+				if (state != null) Melon<Core>.Logger.Msg($"{gameObjectName} - {i} - {state.name}");
+			}
+		}
+
+		public static FsmState GetFsmState(string gameObjectName, int stateIndex) {
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return null;
+
+			if (stateIndex >= fsm.FsmStates.Count) return null;
+
+			return fsm.FsmStates[stateIndex];
+		}
+
+		public static FsmState GetActiveFsmState(string gameObjectName)
+		{
+			PlayMakerFSM fsm = GetPlayMakerFSM(gameObjectName);
+
+			if (fsm == null) return null;
+
+			foreach(FsmState state in fsm.FsmStates)
+			{
+				if (state.active)
+				{
+					return state;
+				}
+			}
+
+			return null;
+		}
     }
 }
